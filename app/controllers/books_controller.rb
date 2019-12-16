0dcebc3885127e7dbca1a5f15c08a9132ab20617
watanabeyosinobu <nobu.regulus4946@gmail.com>
2019-12-16 08:01:31 +0000

    [finish]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+   def create
+   	 @book = Book.new(book_params)
+   	  if @book.save
+      redirect_to book_path(@book), notice:"successfully"
+  else
+  	@books = Book.all
+  	render 'index'
+  end
+ end
+
+   def index
+   	  @book = Book.new
+   	  @books = Book.all
+   end
+
+   def show
+   	  @book = Book.find(params[:id])
+   end
+
+   def edit
+   	  @book = Book.find(params[:id])
+   end
+
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book), notice:"successfully"
+   end
+
+   def destroy
+   	   book = Book.find(params[:id])
+   	   book.destroy
+   	   redirect_to books_path
+   end
+
+   def top
+   end
+
+ private
+ def book_params
+   params.require(:book).permit(:title, :body)
+ end
+end
